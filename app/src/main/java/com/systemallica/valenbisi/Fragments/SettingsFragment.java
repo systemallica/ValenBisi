56d9e9af936d8335a42008cd8f1df3000a7893ae
systemallica <andressser@gmail.com>
2016-10-17 19:15:44 +0200

    Fixed preferences crashing cause I'm dumb

--- app/src/main/java/com/systemallica/valenbisi/Fragments/SettingsFragment.java
+++ app/src/main/java/com/systemallica/valenbisi/Fragments/SettingsFragment.java
@@ -23,7 +23,7 @@ public class SettingsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         // Load the preferences from an XML resource
-        //addPreferencesFromResource(R.xml.fragment_settings);
+        addPreferencesFromResource(R.xml.fragment_settings);
 
         final CheckBoxPreference navBarPref = (CheckBoxPreference) findPreference("navBar");
 
