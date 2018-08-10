0b30a34d008d6c6a5bc2250d4aae9bbd7e6df036
Andrés Reverón Molina <andressser@gmail.com>
2018-08-10 22:32:26 +0200

    refacto: AboutFragment kotlin

---
+++ app/src/main/java/com/systemallica/valenbisi/Fragments/AboutFragment.kt
@@ -0,0 +1,57 @@
+package com.systemallica.valenbisi.fragments
+
+import android.support.v4.app.Fragment
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.systemallica.valenbisi.BuildConfig
+import com.systemallica.valenbisi.R
+
+import kotlinx.android.synthetic.main.fragment_about.*
+
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
+
+        //Change toolbar title
+        activity!!.setTitle(R.string.nav_about)
+
+        //Add version number to the textview
+        versionCode.text = BuildConfig.VERSION_NAME
+
+        setClickListeners()
+
+        return view
+    }
+
+
+    fun setClickListeners() {
+        //Open GitHub page
+        github.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/systemallica/ValenBisi"))
+            startActivity(browserIntent)
+        }
+
+        //Send email
+        email.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.data = Uri.parse("mailto:systemallica.apps@gmail.com")
+            startActivity(emailIntent)
+        }
+
+        // Rate app
+        rate.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.systemallica.valenbisi"))
+            startActivity(browserIntent)
+        }
+    }
+}
+
