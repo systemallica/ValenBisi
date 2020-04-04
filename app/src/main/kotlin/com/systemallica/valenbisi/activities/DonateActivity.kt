33b19046a709d8af68e2c512c4097a06fdfa138f
Andrés Reverón Molina <andressser@gmail.com>
2020-04-04 18:42:33 +0200

    upgrade billing v2

--- app/src/main/kotlin/com/systemallica/valenbisi/activities/DonateActivity.kt
+++ app/src/main/kotlin/com/systemallica/valenbisi/activities/DonateActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import android.util.Log
 import com.android.billingclient.api.*
 import com.systemallica.valenbisi.R
 import kotlinx.android.synthetic.main.activity_donate.*
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 
-class DonateActivity : AppCompatActivity(), PurchasesUpdatedListener {
+class DonateActivity : AppCompatActivity(), PurchasesUpdatedListener, CoroutineScope {
+
+    private lateinit var billingClient: BillingClient
+    private var job: Job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +29,11 @@ class DonateActivity : AppCompatActivity(), PurchasesUpdatedListener {
         supportActionBar!!.setDisplayShowTitleEnabled(false)
 
         setClickListeners()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -36,19 +48,17 @@ class DonateActivity : AppCompatActivity(), PurchasesUpdatedListener {
     }
 
     private fun startBuyProcess(sku: String) {
-        val mBillingClient: BillingClient =
-            BillingClient.newBuilder(applicationContext).setListener(this).build()
-        mBillingClient.startConnection(object : BillingClientStateListener {
-            override fun onBillingSetupFinished(@BillingClient.BillingResponse billingResponseCode: Int) {
-                if (billingResponseCode == BillingClient.BillingResponse.OK) {
-                    // The billing client is ready
+
+        billingClient = BillingClient.newBuilder(applicationContext).setListener(this).enablePendingPurchases().build()
+        billingClient.startConnection(object : BillingClientStateListener {
+            override fun onBillingSetupFinished(billingResult: BillingResult) {
+                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                    // The BillingClient is ready
                     // Start buy process
-                    val flowParams = BillingFlowParams.newBuilder()
-                        .setSku(sku)
-                        .setType(BillingClient.SkuType.INAPP)
-                        .build()
-                    // Launch purchase
-                    mBillingClient.launchBillingFlow(this@DonateActivity, flowParams)
+                    launch {
+                        val result = querySkuDetails(sku)
+                        onResult(result)
+                    }
                 }
             }
 
@@ -56,20 +66,50 @@ class DonateActivity : AppCompatActivity(), PurchasesUpdatedListener {
                 // Try to restart the connection on the next request to
                 // Google Play by calling the startConnection() method.
                 Snackbar.make(donateView!!, R.string.donation_cancelled, Snackbar.LENGTH_SHORT)
-                    .show()
+                        .show()
             }
         })
     }
 
-    override fun onPurchasesUpdated(@BillingClient.BillingResponse responseCode: Int, purchases: List<Purchase>?) {
-        if (responseCode == BillingClient.BillingResponse.OK && purchases != null) {
-            // Apply preference
-            val settings = getSharedPreferences(PREFS_NAME, 0)
-            val editor = settings.edit()
-            editor.putBoolean("donationPurchased", true).apply()
+    private fun onResult(skuDetailsLis: SkuDetailsResult) {
+        // Only one item at a time
+        val skuDetails = skuDetailsLis.skuDetailsList!![0]
 
-            consumePurchases()
-        } else if (responseCode == BillingClient.BillingResponse.USER_CANCELED) {
+        // Set params of the purchase
+        val flowParams = BillingFlowParams.newBuilder()
+                .setSkuDetails(skuDetails)
+                .build()
+
+        // Launch purchase
+        billingClient.launchBillingFlow(this@DonateActivity, flowParams)
+
+    }
+
+    private suspend fun querySkuDetails(sku: String): SkuDetailsResult {
+        val skuList = ArrayList<String>()
+        skuList.add(sku)
+
+        val params = SkuDetailsParams.newBuilder()
+        params.setSkusList(skuList).setType(BillingClient.SkuType.INAPP)
+
+        // Return SkuDetails
+        return withContext(Dispatchers.IO) {
+            billingClient.querySkuDetails(params.build())
+        }
+    }
+
+    override fun onPurchasesUpdated(billingResult: BillingResult, purchases: List<Purchase>?) {
+        if (billingResult.responseCode == BillingClient.BillingResponseCode.OK && purchases != null) {
+            // Acknowledge purchase
+            launch {
+                handlePurchase(purchases[0])
+            }
+
+            // Consume it so it can be purchased again
+            launch {
+                consumePurchase(purchases[0])
+            }
+        } else if (billingResult.responseCode == BillingClient.BillingResponseCode.USER_CANCELED) {
             // Handle an error caused by a user cancelling the purchase flow.
             Snackbar.make(donateView!!, R.string.donation_cancelled, Snackbar.LENGTH_SHORT).show()
         } else {
@@ -78,35 +118,28 @@ class DonateActivity : AppCompatActivity(), PurchasesUpdatedListener {
         }
     }
 
-    private fun consumePurchases(){
-        val mBillingClient: BillingClient =
-            BillingClient.newBuilder(applicationContext).setListener(this).build()
-        mBillingClient.startConnection(object : BillingClientStateListener {
-            override fun onBillingSetupFinished(@BillingClient.BillingResponse billingResponseCode: Int) {
-                if (billingResponseCode == BillingClient.BillingResponse.OK) {
-                    // The billing client is ready
-                    val purchasesResult = mBillingClient.queryPurchases(BillingClient.SkuType.INAPP)
-                    val purchases = purchasesResult.purchasesList
-
-                    val consumeListener = { responseCodeConsumed: Int, purchaseToken: String ->
-                        if (responseCodeConsumed == BillingClient.BillingResponse.OK) {
-                            Log.i("Billing", purchaseToken)
-                        }
-                    }
-
-                    for(purchase in purchases){
-                        mBillingClient.consumeAsync(
-                            purchase.purchaseToken, consumeListener
-                        )
-                    }
+    private suspend fun handlePurchase(purchase: Purchase) {
+        if (purchase.purchaseState == Purchase.PurchaseState.PURCHASED) {
+            // Grant entitlement to the user.
+            Snackbar.make(donateView!!, "Thank you!", Snackbar.LENGTH_SHORT).show()
+            // Acknowledge the purchase if it hasn't already been acknowledged.
+            if (!purchase.isAcknowledged) {
+                val acknowledgePurchaseParams = AcknowledgePurchaseParams.newBuilder()
+                        .setPurchaseToken(purchase.purchaseToken)
+                withContext(Dispatchers.IO) {
+                    billingClient.acknowledgePurchase(acknowledgePurchaseParams.build())
                 }
             }
+        }
+    }
 
-            override fun onBillingServiceDisconnected() {
-                // Try to restart the connection on the next request to
-                // Google Play by calling the startConnection() method.
-                Log.e("Billing", "consume failed")
-            }
-        })
+    private suspend fun consumePurchase(purchase: Purchase) {
+        val consumeParams =
+                ConsumeParams.newBuilder()
+                        .setPurchaseToken(purchase.purchaseToken)
+                        .build()
+        withContext(Dispatchers.IO) {
+            billingClient.consumePurchase(consumeParams)
+        }
     }
 }
